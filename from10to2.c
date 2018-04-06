1ba9a1959f4f4309ff30edf398abcc3e577b1fd0
Alexandra <32397047+Littlesweetycupoftea@users.noreply.github.com>
2018-04-07 00:30:49 +0300

    Update from10to2.c

--- from10to2.c
+++ from10to2.c
@@ -1,22 +1,20 @@
-#include <stdio.h>  
-#include <math.h> 
+#include <stdio.h> 
+#include < math.h >
 
-void dvoich(int x1)  
-{ int i, b; 
-int c = 0; 
-for(i=0; x1 > 0; i++) 
-{ b = x1 % 2; 
-x1= (x1-b)/2;
-c += b * pow(10,i);
+  void dvoich(int x1) {
+        int i, b;
+        int c = 0;
+        for (i = 0; x1 > 0; i++) {
+            b = x1 % 2;
+            x1 = (x1 - b) / 2;
+            c += b * pow(10, i);
+        }
+        printf("Число в двоичной системе: %d\n", c);
+    }
+
+  void main() {
+    int a;
+    printf("Введите число в 10 системе:\n");
+    scanf("%d", & a);
+    dvoich(a);
 }
-
-printf("Dvoichnoe chislo: %d\n",c);  
-}  
-
-void main()  
-{
-int a; 
-printf("Vvedite desyatichnoe chislo:\n"); 
-scanf("%d",&a); 
-dvoich(a); 
-} 
